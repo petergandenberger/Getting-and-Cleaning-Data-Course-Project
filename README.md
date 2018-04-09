78179ac5d160ea19510d1960f726b46f524acccd
Peter Gandenberger <peter.gandenberger@gmail.com>
2018-04-09 16:15:30 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# Getting and Cleaning Data Course Project
+## Description
+The Repo contains the files for the final Project of the coursera course "Getting and Cleaning Data". For more Information about the variables please refer to the Codebook.md file.
+
+## Script
+The "run_analysis.R" Script does the following:
+1. Merges the training and the test sets to create one data set.
+2. Extracts only the measurements on the mean and standard deviation for each measurement.
+3. Uses descriptive activity names to name the activities in the data set
+4. Appropriately labels the data set with descriptive variable names.
+5. From the data set in step 4, creates a second, independent tidy data set with the average of each variable for each activity and each subject.
+
+It looks for the data in the "/UCI HAR Dataset" folder.
+For more Information about the script please refer to the comments in the script itself.
---
+++ run_analysis.R
@@ -0,0 +1,45 @@
+## Read the txt-files for training and test Datasets
+test.data <- read.table("UCI HAR Dataset/test/X_test.txt")
+test.activities <- read.table("UCI HAR Dataset/test/y_test.txt")
+test.subjects <- read.table("UCI HAR Dataset/test/subject_test.txt")
+train.data <- read.table("UCI HAR Dataset/train/X_train.txt")
+train.activities <- read.table("UCI HAR Dataset/train/y_train.txt")
+train.subjects <- read.table("UCI HAR Dataset/train/subject_train.txt")
+
+## Read the features.txt file
+features <- read.table("UCI HAR Dataset/features.txt")
+names(features) <- c("index", "featureName")
+
+## Rename the test and training data-names according to the features
+names(test.data) <- features[,2]
+names(train.data) <- features[,2]
+
+## find the features for mean or standard deviations
+features$meanStd <- str_detect(features[,2], "mean|std")
+
+## select only the columns in the combined.data that are mean or standard deviation features
+test.meanStd <- test.data[, features$meanStd]
+train.meanStd <- train.data[, features$meanStd]
+
+## Read the activity labels 
+activity.labels <- read.table("UCI HAR Dataset/activity_labels.txt")
+train.activities.labels <- left_join(train.activities, activity.labels, by = "V1")[, "V2"]
+test.activities.labels <- left_join(test.activities, activity.labels, by = "V1")[, "V2"]
+
+## add the subject ids to the datasets
+test.meanStd <- cbind("subjectid" = unlist(test.subjects), test.meanStd)
+train.meanStd <- cbind("subjectid" = unlist(train.subjects), train.meanStd)
+
+
+## add the activity labels to the datasets
+test.meanStd <- cbind("activity" = test.activities.labels, test.meanStd)
+train.meanStd <- cbind("activity" = train.activities.labels, train.meanStd)
+
+## combine the training and test data
+combined.data <- union(test.meanStd, train.meanStd)
+
+## clean the variable names
+names(combined.data) <- str_to_lower(str_replace_all(names(combined.data), "\\(|\\)|-", ""))
+
+## calculate the mean of the variables by activity and subjectId
+combined.avg <- combined.data %>% group_by(activity, subjectid) %>% summarise_all(funs(mean))
